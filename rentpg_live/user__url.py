030d3ca0c4ed26dc1f97d30e98221825a582f726
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-06-09 16:09:50 +0530

    Add user URL routing for login, dashboard, logout, and registration

---
+++ rentpg_live/user__url.py
@@ -0,0 +1,11 @@
+# rentpg_live/admin__url.py
+
+from django.urls import path
+from rentpg_live import user__web
+
+urlpatterns = [
+    path('user/login/', user__web.userLogin, name='user_login'),
+    path('user/dashboard/', user__web.userDashboard, name='user_dashboard'),
+    path('user/logout/', user__web.userLogout, name='user_logout'),
+    path('user/register/', user__web.userRegister, name='register'),
+]

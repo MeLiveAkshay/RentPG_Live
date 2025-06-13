6ff9bff4b89b09ebc8b848774b639de18b7d87ca
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-06-13 09:31:53 +0530

    Add User model and initial migrations for service app

--- service/models.py
+++ service/models.py
@@ -6,3 +6,15 @@ class ExampleModel(models.Model):
     icon=models.CharField(max_length=100, default='fa fa-cog')
     created_at = models.DateTimeField(auto_now_add=True)
    
+
+
+class User(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    username = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    icon = models.CharField(max_length=100, default='fa fa-user')
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    

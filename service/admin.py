fb4d3e2b498467293d1faa5264d1740afcb4406a
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-06-13 10:13:41 +0530

    Add User and ExampleModel admin configurations, enhance User model with new fields

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from service.models import User, ExampleModel
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_number', 'present_address', 'profession', 'created_at')
+    search_fields = ('name', 'email', 'phone_number')
 
 # Register your models here.
+@admin.register(ExampleModel)
+class ExampleModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'icon', 'created_at')
+    search_fields = ('name', 'description')
+admin.site.site_header = "RentPG Admin"
+admin.site.site_title = "RentPG Admin Portal"
--- service/models.py
+++ service/models.py
@@ -15,6 +15,9 @@ class User(models.Model):
     password = models.CharField(max_length=128)
     icon = models.CharField(max_length=100, default='fa fa-user')
     phone_number = models.CharField(max_length=15, null=True, blank=True)
+    present_address = models.CharField(max_length=255, null=True, blank=True)
+    profession = models.CharField(max_length=100, null=True, blank=True)
+    parament_address = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     

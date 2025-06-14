0132bc63879704b8aab79b070c68c99a0c9f4778
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-06-14 11:08:21 +0530

    Remove unused admin, models, views, and tests files; add AdminUser, ContactUs, and TeamMember models with admin configurations

--- service/admin.py
+++ service/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from service.models import User, ExampleModel
+from service.models import User, ExampleModel, AdminUser, ContactUs, TeamMember
+
+@admin.register(AdminUser)
+class AdminUserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_number', 'created_at')
+    search_fields = ('name', 'email', 'phone_number')
+@admin.register(ContactUs)
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_at')
+    search_fields = ('name', 'email', 'subject')
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone_number', 'present_address', 'profession', 'created_at')
@@ -15,3 +24,6 @@ class ExampleModelAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description')
 admin.site.site_header = "RentPG Admin"
 admin.site.site_title = "RentPG Admin Portal"
+
+admin.site.index_title = "Welcome to RentPG Admin Portal"
+
